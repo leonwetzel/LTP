14d3470c1bb6432167d2a1759baaeadbf4ae6d01
Leon Wetzel <leonwetzel@gmail.com>
2021-06-07 11:29:24 +0200

    Minor changes to auxiliary.py

--- auxiliary.py
+++ auxiliary.py
@@ -13,11 +13,12 @@ __status__ = "Development"
 import numpy as np
 import pandas as pd
 
+import torch
 from torch.utils.data import Dataset
 
 
 class SentenceDataset(Dataset):
-    def __init__(self, data_file):
+    def __init__(self, data_file, tokenizer):
         super().__init__()
 
         self.corpus_frame = pd.read_csv(data_file)
@@ -30,7 +31,35 @@ class SentenceDataset(Dataset):
     def __len__(self):
         return len(self.corpus_frame)
 
-    def __getitem__(self, item):
+    def __getitem__(self, index):
         # TODO: ervoor zorgen dat een Tensor-object geretourneerd wordt
         # voorbeeld: torch.Tensor(self.data[index]).long(), torch.Tensor(self.labels[index]).long()
-        return item
+        return index
+
+
+def padding_collate_fn(batch):
+    """
+    Pads data with zeros to size of longest sentence in batch.
+
+    Parameters
+    ----------
+    batch : tuple
+        Part of the data that should be padded.
+
+    Returns
+    -------
+    padded_data :
+        asdladjkasd
+    padded_labels :
+        asdlsadjsadsad
+
+    """
+    data, labels = zip(*batch)
+    largest_sample = max([len(d) for d in data])
+    padded_data = torch.zeros((len(data), largest_sample), dtype=torch.long)
+    padded_labels = torch.full_like(padded_data, -100)  # TODO: replace by valid ignore index
+    for i, sample in enumerate(data):
+        padded_data[i, :len(sample)] = sample
+        padded_labels[i, :len(sample)] = labels[i]
+
+    return padded_data, padded_labels
